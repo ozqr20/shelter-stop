a0f05e194df61616173e8ed3c0a1ad548e36a363
Oscar <53874145+ozqr20@users.noreply.github.com>
2022-07-22 01:15:11 -0400

    starting function petRead

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,27 @@
+const pets = [];
 // add key as variable for petfinder
 var key = "kDth6aBoA1LVzzoiCjQkPR3AVoOOEChNhYGXwylaEOPZcrrVrm";
 
 //test
+var saveSearch = function(){
+    localStorage.setItem("pets", JSON.stringify(cities));
+};
+
+
+var petRead = function(city){
+    var apiUrl = ``  // https goes here 
+
+    fetch(apiUrl).then(function(response){
+        if(response.ok){
+            console.log(response);
+            response.json().then(function(data){
+                console.log(data);
+            });
+        }else{
+            alert("Error");
+        }   
+    })
+    .catch(function(error){
+        alert("Unable to connect");
+    });
+};
